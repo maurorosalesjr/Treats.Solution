2b288ebd94af5ce5b888b1dd9b1ff3a601c06864
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-08-25 16:26:38 -0700

    successfully calls all  treats with flavors to home index

--- Pierre/Controllers/HomeController.cs
+++ Pierre/Controllers/HomeController.cs
@@ -1,14 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pierre.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Pierre.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly PierreContext _db;
+
+      public HomeController(PierreContext db)
+      {
+        _db = db;
+      }
 
       [HttpGet("/")]
       public ActionResult Index()
       {
-        return View();
+        return View(_db.FlavorTreat.ToList());
       }
 
     }
